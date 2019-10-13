5141e4f9e315ea7c34a1ad8480cb642983225ce6
Nathan Ifill <nathan.ifill@live.co.uk>
2019-10-13 10:48:57 +0000

    Initialise repository

---
+++ index.js
@@ -0,0 +1,23 @@
+// // Reverses a string.
+// function reverse(string) {
+//   return Array.from(string).reverse().join("");
+// }
+
+// Adds 'reverse' to all strings
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object
+function Phrase(content) {
+  this.content = content;
+
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
